1be107bb228d743e610fb674359a1b0b5086834a
Victor Colina <victorcolina@G500606MAC272.local>
2024-10-23 22:39:08 -0300

    feat: truncating description text home screen

--- src/components/MangaThumb.tsx
+++ src/components/MangaThumb.tsx
@@ -3,22 +3,14 @@ export function MangaThumb({ id, title, image }: any) {
     <div class='flex flex-col gap-2 w-[150px] reveal-animation'>
       <a href={`/book/${id}`}>
         <img
-          class='w-full object-cover rounded-xl hover:scale-105 transition-transform cursor-pointer'
+          class='min-h-[230px] w-full object-cover rounded-xl hover:scale-105 transition-transform cursor-pointer'
           src={image}
           alt={title}
           // transition:name={`image-thumb-${id}`}
         />
       </a>
 
-      <p class='text-xl'>{title}</p>
-
-      {/* <div class="flex items-center gap-2 flex-wrap w-[130px]"> */}
-      {/*   {tags.map((tag: any) => ( */}
-      {/*     <div class="border border-[#B6C4B6] rounded-md px-2"> */}
-      {/*       <p class="text-xs text-gray-500">{tag.name.en}</p> */}
-      {/*     </div> */}
-      {/*   ))} */}
-      {/* </div> */}
+      <p class='text-md line-clamp-5 overflow-hidden'>{title}</p>
     </div>
   );
 }
